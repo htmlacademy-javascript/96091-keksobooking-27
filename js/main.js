8507ff4fcb8351fbd3ce38d2dd81cc27920ebb08
SergViktKobzev <serg-vikt.kobzev@mail.ru>
2022-10-12 09:21:38 +0300

    Заменил свои функции из прошлого задания на  эталонную  реализацию

--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function getIntegerFromRange (minValue, maxValue) {
-  if (minValue < 0 || typeof(minValue) !== 'number') { return NaN;}
-  if (maxValue < 0 || typeof(maxValue) !== 'number') { return NaN;}
-  if (minValue === maxValue) {return minValue;}
-  if (minValue > maxValue) {
-    const correctMinValue = maxValue;
-    const correctMaxValue = minValue;
-    minValue = correctMinValue;
-    maxValue = correctMaxValue;
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+function getRandomPositiveInteger (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
   }
-  const result = Math.floor(minValue + ((maxValue - minValue + 1) * Math.random()));
-  return result;
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
-getIntegerFromRange(0, 100);
+getRandomPositiveInteger(0, 100);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getDecimalFromRange (minValue, maxValue, numbersAfterComma) {
-  if (minValue < 0 || typeof(minValue) !== 'number') { return NaN;}
-  if (maxValue < 0 || typeof(maxValue) !== 'number') { return NaN;}
-  if (numbersAfterComma < 0 || (numbersAfterComma % 1 !== 0) || typeof(minValue) !== 'number') { return NaN;}
-  if (minValue === maxValue) {return minValue;}
-  if (minValue > maxValue) {
-    const correctMinValue = maxValue;
-    const correctMaxValue = minValue;
-    minValue = correctMinValue;
-    maxValue = correctMaxValue;
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+function getRandomPositiveFloat (a, b, digits = 1) {
+  if (a < 0 || b < 0 || digits < 0) {
+    return NaN;
   }
-  const randomValue = minValue + ((maxValue - minValue) * Math.random());
-  numbersAfterComma = Math.pow(10, numbersAfterComma);
-  const result = Math.floor(randomValue * numbersAfterComma) / numbersAfterComma;
-  return result;
+  const lower = Math.min(a, b);
+  const upper = Math.max(a, b);
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
 }
-getDecimalFromRange(0, 100, 3);
+getRandomPositiveFloat(0, 100, 3);
