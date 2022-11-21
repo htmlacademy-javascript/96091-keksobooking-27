94a285d1d7772d27b20d684f4bb5e8d400396222
SergViktKobzev <serg-vikt.kobzev@mail.ru>
2022-11-21 19:19:17 +0300

    Исправил замечания наставника

--- js/api.js
+++ js/api.js
@@ -1,12 +1,14 @@
 import {showAlert} from './util.js';
 import {setDefaultOffers} from './filter.js';
 import {setActiveFilter} from './form.js';
+const URL_FOR_REQUEST = 'https://27.javascript.pages.academy/keksobooking/data';
+const URL_FOR_SEND = 'https://27.javascript.pages.academy/keksobooking';
 const ALERT_SHOW_TIME = 5000;
 const MESSAGE = 'Ошибка загрузки! Перезагрузите страницу!';
 
 
 function getOffers() {
-  fetch('https://27.javascript.pages.academy/keksobooking/data')
+  fetch(URL_FOR_REQUEST)
     .then((response) => response.json())
     .then((allOffers) => setDefaultOffers(allOffers))
     .then(() => setActiveFilter())
@@ -15,7 +17,7 @@ function getOffers() {
 
 function sendOffer(body, onSuccess, onFail) {
   fetch(
-    'https://27.javascript.pages.academy/keksobooking',
+    URL_FOR_SEND,
     {
       method: 'POST',
       body
